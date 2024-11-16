6ab9ed32841858858ca8ada86e800ab82e26cd9a
dtzr09 <derricktay17@gmail.com>
2024-11-16 16:08:45 +0800

    delete csv, update docs and readme

--- README.md
+++ README.md
@@ -25,6 +25,7 @@
 - [x] View all hospital staffs
 - [x] Add new hospital staffs
 - [x] Delete hospital staffs
+- [x] View the appointment details of the doctors
 - [x] View all medications
 - [x] Delete medication
 - [x] Add new medication
@@ -32,57 +33,43 @@
 - [x] View pending medication replenishment request
 - [x] Approve medication replenishment request
 - [x] View profile
+- [x] Update profile
 - [x] Change password
 
-## Example of a new administrator registration workflow
+### Patient
 
-```
-System.out.print("Are you a new user? [y/n] ");
-    if (CustScanner.getStrChoice().equalsIgnoreCase("y")) {
-        RegisterDisplay.registerDisplay();
-        }
-```
+- [x] View medical record
+- [x] View the available appointment slots for his/her doctor
+- [x] Schedule appointment
+- [x] Reschedule appointment
+- [x] Cancel appointment
+- [x] View scheduled appointments
+- [x] View pending appointments
+- [x] View past appointment outcome records
+- [x] View profile
+- [x] Update profile
+- [x] Change password
 
-In `WelcomeDisplay.java`, user will respond yes when prompted if they are a new user, which will then bring them the `registerDisplay` function of `RegisterDisplay` class.
+### Doctor
 
-```
-userType = switch (choice) {
-                case 1 -> UserType.DOCTOR;
-                case 2 -> UserType.PATIENT;
-                case 3 -> UserType.PHARMACIST;
-                case 4 -> UserType.ADMINISTRATOR;
-                default -> throw new IllegalStateException("Unexpected value: " + choice);
-            };
-            registerUserDisplay(userType);
-```
+- [x] View his/her patients
+- [x] View his/her patients medical records
+- [x] Update his/her patient medical record
+- [x] Set availability for appointment slots
+- [x] Manage appointment requests
+- [x] View scheduled appointments
+- [x] Record appointment outcome
+- [x] View profile
+- [x] Update profile
+- [x] Change password
 
-User will then input that they want to register as a new administrator (choice number 4). A variable `userType` will be then be assigned `UserType.ADMINISTRATOR`. `registerUserDisplay` will then be called with the `UserType.ADMINISTRATOR` as argument.
+### Pharmacist
 
-```
-User user = AccountManager.register(userEmail, name, userType);
-```
-
-In `registerUserDisplay`, the user will be asked to enter their name and email address. It will then attempt to register the new user using the `register()` function in the `AccountManager` class by passing it the email, name as well as the userType.
-
-```
-User user = UserManager.createUser(email, name, userType, "password");
-return user;
-```
-
-The `createUser` function in `UserManager` class will then be called with a default password `password` to be saved in the "database" and the user of type `User` will be returned.
-
-```
- if (PasswordManager.checkPassword(user, "password")) {
-        System.out.print("Please enter a new password: ");
-        try {
-            PasswordManager.changePassword(user, "password", CustScanner.getPassword());
-        } catch (Exception e) {
-            System.out.println("Password change failed. Please try again.");
-            PasswordManager.changePassword(user, "password", CustScanner.getPassword());
-        }
-        UserManager.updateUser(user);
-        System.out.println("Password changed successfully.");
-    }
-```
-
-Once the user has been registered in our "database", the user will be prompted to change his password from the default password. The responsibility for changing password will then be passed to the `PasswordManager`. Once the password has been successfully changed, the system will then update the user by calling `UserManager.updateUser()`.
+- [x] View all the appointment outcome records
+- [x] Update prescription status for the appointments
+- [x] View medication inventory
+- [x] View low stock medication inventory
+- [x] Submit request to replenish low stock medication 
+- [x] View profile
+- [x] Update profile
+- [x] Change password
--- controller/appointment/AppointmentOutcomeManager.java
+++ controller/appointment/AppointmentOutcomeManager.java
@@ -15,8 +15,10 @@ import model.appointment.enums.AppointmentOutcomeStatus;
 import model.diagnosis.Diagnosis;
 import model.prescription.Prescription;
 import utils.exceptions.PageBackException;
+
 /**
- * The AppointmentOutcomeManager class provides utility methods for managing Appointment Outcomes such as recording and retrieving.
+ * The AppointmentOutcomeManager class provides utility methods for managing
+ * Appointment Outcomes such as recording and retrieving.
  */
 
 public class AppointmentOutcomeManager {
@@ -24,12 +26,16 @@ public class AppointmentOutcomeManager {
     /**
      * Creates a new appointment outcome and adds it to the database.
      * 
-     * This method takes an `AppointmentOutcome` object, attempts to add it to the `AppointmentOutcomeDatabase`, 
-     * and handles any exceptions that may arise during the process. If the outcome is successfully added, it 
-     * remains stored in the database for future reference. If an error occurs during the operation, an error 
+     * This method takes an `AppointmentOutcome` object, attempts to add it to the
+     * `AppointmentOutcomeDatabase`,
+     * and handles any exceptions that may arise during the process. If the outcome
+     * is successfully added, it
+     * remains stored in the database for future reference. If an error occurs
+     * during the operation, an error
      * message is displayed.
      * 
-     * @param appointmentOutcome The `AppointmentOutcome` object to be added to the database.
+     * @param appointmentOutcome The `AppointmentOutcome` object to be added to the
+     *                           database.
      */
     public static void createNewAppointmentOutcome(AppointmentOutcome appointmentOutcome) {
         try {
@@ -42,19 +48,26 @@ public class AppointmentOutcomeManager {
     /**
      * Updates the details of an existing appointment outcome and adds a diagnosis.
      * 
-     * This method updates the given `AppointmentOutcome` object by setting the type of service, 
-     * diagnosis ID, and consultation notes. The status of the appointment outcome is then updated 
-     * to "COMPLETED". The updated outcome is stored in the `AppointmentOutcomeDatabase`, and the 
+     * This method updates the given `AppointmentOutcome` object by setting the type
+     * of service,
+     * diagnosis ID, and consultation notes. The status of the appointment outcome
+     * is then updated
+     * to "COMPLETED". The updated outcome is stored in the
+     * `AppointmentOutcomeDatabase`, and the
      * provided diagnosis is added to the `DiagnosisManager`.
      * 
-     * If any error occurs during the update process, the exception is caught, and an error message 
+     * If any error occurs during the update process, the exception is caught, and
+     * an error message
      * is printed to the console.
      * 
      * @param appointmentOutcome The `AppointmentOutcome` object to be updated.
-     * @param diagnosis The `Diagnosis` object to be added to the system.
-     * @param typeOfService The type of medical service provided during the appointment.
-     * @param diagnosisID The ID of the diagnosis associated with the appointment.
-     * @param consultationNotes Notes regarding the consultation or findings during the appointment.
+     * @param diagnosis          The `Diagnosis` object to be added to the system.
+     * @param typeOfService      The type of medical service provided during the
+     *                           appointment.
+     * @param diagnosisID        The ID of the diagnosis associated with the
+     *                           appointment.
+     * @param consultationNotes  Notes regarding the consultation or findings during
+     *                           the appointment.
      */
     public static void updateAppointmentOutcome(AppointmentOutcome appointmentOutcome, Diagnosis diagnosis,
             String typeOfService, String diagnosisID, String consultationNotes) {
@@ -74,8 +87,10 @@ public class AppointmentOutcomeManager {
     /**
      * Retrieves all appointment outcomes from the database.
      * 
-     * This method fetches and returns a list of all `AppointmentOutcome` objects stored in the 
-     * `AppointmentOutcomeDatabase`. The list contains the details of all outcomes recorded for 
+     * This method fetches and returns a list of all `AppointmentOutcome` objects
+     * stored in the
+     * `AppointmentOutcomeDatabase`. The list contains the details of all outcomes
+     * recorded for
      * appointments.
      * 
      * @return A list of all `AppointmentOutcome` objects in the database.
@@ -83,15 +98,20 @@ public class AppointmentOutcomeManager {
     public static List<AppointmentOutcome> getAllAppointmentOutcome() {
         return AppointmentOutcomeDatabase.getDB().getAllAppointmentOutcomes();
     }
+
     /**
      * Retrieves the appointment outcome for a given appointment ID.
      * 
-     * This method searches through all appointment outcomes in the database to find the outcome 
-     * associated with the provided appointment ID. If the outcome is found, it is returned; 
+     * This method searches through all appointment outcomes in the database to find
+     * the outcome
+     * associated with the provided appointment ID. If the outcome is found, it is
+     * returned;
      * otherwise, the method returns null.
      * 
-     * @param appointmentID The ID of the appointment whose outcome is to be retrieved.
-     * @return The `AppointmentOutcome` object associated with the given appointment ID, or null 
+     * @param appointmentID The ID of the appointment whose outcome is to be
+     *                      retrieved.
+     * @return The `AppointmentOutcome` object associated with the given appointment
+     *         ID, or null
      *         if no such outcome is found.
      */
     public static AppointmentOutcome getAppointmentOutcomeByAppointmentID(String appointmentID) {
@@ -111,12 +131,16 @@ public class AppointmentOutcomeManager {
     /**
      * Retrieves all appointment outcome records for a given patient.
      * 
-     * This method filters and returns a list of all `AppointmentOutcome` objects that are associated 
-     * with the specified patient ID. It searches through all appointment outcomes and adds those 
+     * This method filters and returns a list of all `AppointmentOutcome` objects
+     * that are associated
+     * with the specified patient ID. It searches through all appointment outcomes
+     * and adds those
      * matching the given patient ID to the result list.
      * 
-     * @param patientID The ID of the patient whose appointment outcomes are to be retrieved.
-     * @return A list of `AppointmentOutcome` objects associated with the given patient ID, 
+     * @param patientID The ID of the patient whose appointment outcomes are to be
+     *                  retrieved.
+     * @return A list of `AppointmentOutcome` objects associated with the given
+     *         patient ID,
      *         or null if an error occurs during the process.
      */
     public static List<AppointmentOutcome> getPatientsAppointmentOutcomeRecords(String patientID) {
@@ -136,17 +160,25 @@ public class AppointmentOutcomeManager {
     }
 
     /**
-     * Retrieves a list of appointment outcome records for a given list of appointment outcomes and patient ID.
+     * Retrieves a list of appointment outcome records for a given list of
+     * appointment outcomes and patient ID.
      * 
-     * This method processes a list of `AppointmentOutcome` objects, fetches the corresponding appointment, diagnosis,
-     * and prescription details, and compiles this information into a list of `AppointmentOutcomeRecord` objects. Each record
-     * contains detailed information about the appointment outcome, including type of service, consultation notes, 
+     * This method processes a list of `AppointmentOutcome` objects, fetches the
+     * corresponding appointment, diagnosis,
+     * and prescription details, and compiles this information into a list of
+     * `AppointmentOutcomeRecord` objects. Each record
+     * contains detailed information about the appointment outcome, including type
+     * of service, consultation notes,
      * medication names, and prescription status.
      * 
-     * @param recordList The list of `AppointmentOutcome` objects that represent the outcomes of various appointments.
-     * @param patientID The ID of the patient whose appointment outcomes are being processed.
-     * @return A list of `AppointmentOutcomeRecord` objects, each representing the details of an appointment outcome.
-     * @throws PageBackException If there is an error in fetching related data or processing any of the outcomes.
+     * @param recordList The list of `AppointmentOutcome` objects that represent the
+     *                   outcomes of various appointments.
+     * @param patientID  The ID of the patient whose appointment outcomes are being
+     *                   processed.
+     * @return A list of `AppointmentOutcomeRecord` objects, each representing the
+     *         details of an appointment outcome.
+     * @throws PageBackException If there is an error in fetching related data or
+     *                           processing any of the outcomes.
      */
     public static List<AppointmentOutcomeRecord> getAppointmentOutcomeRecords(List<AppointmentOutcome> recordList,
             String patientID) throws PageBackException {
@@ -173,11 +205,15 @@ public class AppointmentOutcomeManager {
     /**
      * Retrieves a list of appointment outcomes for a specific doctor.
      * 
-     * This method filters the list of all appointment outcomes and returns only those that are associated with the given doctor ID.
-     * The outcomes returned represent the results of appointments attended by the specified doctor.
+     * This method filters the list of all appointment outcomes and returns only
+     * those that are associated with the given doctor ID.
+     * The outcomes returned represent the results of appointments attended by the
+     * specified doctor.
      * 
-     * @param doctorID The ID of the doctor whose appointment outcomes are to be retrieved.
-     * @return A list of `AppointmentOutcome` objects that belong to the specified doctor.
+     * @param doctorID The ID of the doctor whose appointment outcomes are to be
+     *                 retrieved.
+     * @return A list of `AppointmentOutcome` objects that belong to the specified
+     *         doctor.
      */
     public static ArrayList<AppointmentOutcome> getAppointmentOutcomeByDoctorID(String doctorID) {
         ArrayList<AppointmentOutcome> doctorAppointmentOutcome = new ArrayList<>();
@@ -191,13 +227,17 @@ public class AppointmentOutcomeManager {
     }
 
     /**
-     * Retrieves the number of completed appointment outcomes for a specific patient.
+     * Retrieves the number of completed appointment outcomes for a specific
+     * patient.
      * 
-     * This method iterates through all appointment outcomes and counts how many are associated
+     * This method iterates through all appointment outcomes and counts how many are
+     * associated
      * with the given patient ID and have a status of "COMPLETED".
      * 
-     * @param patientID The ID of the patient whose completed appointment outcomes are to be counted.
-     * @return The number of completed appointment outcomes for the specified patient.
+     * @param patientID The ID of the patient whose completed appointment outcomes
+     *                  are to be counted.
+     * @return The number of completed appointment outcomes for the specified
+     *         patient.
      */
     public static int getNumberOfAppointmentOutcomeByPatientID(String patientID) {
         List<AppointmentOutcome> appointmentOutcomes = getAllAppointmentOutcome();
